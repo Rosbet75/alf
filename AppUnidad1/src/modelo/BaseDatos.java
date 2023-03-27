f851a201b80cfa66f8aa35ea9cab9157792aa735
Rosbet75 <rosbet75>
2023-03-27 13:49:27 -0600

    classssssss

---
+++ AppUnidad1/src/modelo/BaseDatos.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class BaseDatos {
+
+	private static BaseDatos baseDatos;
+	private String nombre;
+	private String usuario;
+	private String password;
+	private String protocolo;
+	private String driver;
+	
+}
